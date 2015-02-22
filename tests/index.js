45d21ab4241ac64ee3ad4c675b000b054d01007b
robert mcguinness <robert.mcguinness.iii@gmail.com>
2015-02-22 08:48:35 -0500

    add specs

---
+++ tests/index.js
@@ -0,0 +1,33 @@
+/* global describe, it */
+
+var chai = require('chai');
+var metalsmith = require('metalsmith');
+var metalsmithPrism = require('../lib');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var path = require('path');
+var expect = chai.expect;
+
+describe('metalsmith-prism', function() {
+
+  it('should highlight code blocks in html files', function(done) {
+
+    var metal = metalsmith(path.join(__dirname, 'fixtures'));
+
+    metal
+      .use(metalsmithPrism())
+      .build(function(err){
+
+        if(err) {
+          return done(err);
+        }
+
+        var buildContent = fs.readFileSync(path.join(__dirname, 'fixtures/build/markup.html'), "utf8");
+        var expectedContent  = fs.readFileSync(path.join(__dirname, 'fixtures/expected/markup.html'), "utf8");
+
+        expect(buildContent).to.be.eql(expectedContent);
+
+        done();
+      });
+  });
+});
